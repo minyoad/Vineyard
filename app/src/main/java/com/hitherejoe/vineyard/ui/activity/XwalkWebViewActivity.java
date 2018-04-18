a3ddbe15c5a276a367e1ab8e7ff8778ef2692c50
minyoad <minyoad@gmail.com>
2018-04-18 12:10:25 +0800

    add video loading view

--- app/src/main/java/com/hitherejoe/vineyard/ui/activity/XwalkWebViewActivity.java
+++ app/src/main/java/com/hitherejoe/vineyard/ui/activity/XwalkWebViewActivity.java
@@ -3,6 +3,8 @@ package com.hitherejoe.vineyard.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 
@@ -21,6 +23,7 @@ import timber.log.Timber;
 public class XwalkWebViewActivity extends AppCompatActivity {
     XWalkView mXwalkView;
 
+    View loadingView;
     class MyResourceClient extends XWalkResourceClient {
         MyResourceClient(XWalkView view) {
             super(view);
@@ -38,17 +41,94 @@ public class XwalkWebViewActivity extends AppCompatActivity {
 
         }
 
-//        @Override
-//        public WebResourceResponse shouldInterceptLoadRequest(XWalkView view, String url) {
-//            Timber.d("request:"+url);
-//
-//            return super.shouldInterceptLoadRequest(view, url);
-//        }
+
     }
 
     class MyUIClient extends XWalkUIClient {
+
+        XWalkView mWalkView;
+        private void pause(){
+            String js="javascript:var v = document.getElementsByTagName(\"video\")[0];\n" +
+                    "v.paused?v.play():v.pause();";
+
+
+            mWalkView.loadUrl(js);
+        }
+
+        private void forward(){
+
+        }
+
+        private void backward(){
+
+        }
+
+        private void play(){
+            String js="javascript:var v = document.getElementsByTagName(\"video\")[0];\n" +
+                    "v.play();";
+            mWalkView.loadUrl(js);
+        }
+
+
+
         MyUIClient(XWalkView view) {
             super(view);
+            mWalkView=view;
+        }
+
+
+        @Override
+        public void onPageLoadStopped(XWalkView view, String url, LoadStatus status) {
+            super.onPageLoadStopped(view, url, status);
+
+            play();
+
+            loadingView.setVisibility(View.GONE);
+
+        }
+
+        @Override
+        public boolean shouldOverrideKeyEvent(XWalkView view, KeyEvent event) {
+
+            boolean overrided=true;
+            switch (event.getKeyCode()) {
+                case KeyEvent.KEYCODE_DPAD_UP://向上
+//                    Log.e("jamie","－－－－－向上－－－－－");
+                    break;
+                case KeyEvent.KEYCODE_DPAD_DOWN://向下
+//                    Log.e("jamie","－－－－－向下－－－－－");
+                    break;
+                case KeyEvent.KEYCODE_DPAD_LEFT://向左
+//                    Log.e("jamie","－－－－－向左－－－－－");
+                    backward();
+                    break;
+                case KeyEvent.KEYCODE_DPAD_RIGHT://向右
+//                    Log.e("jamie","－－－－－向右－－－－－");
+                    forward();
+                    break;
+                case KeyEvent.KEYCODE_ENTER://确定
+//                    Log.e("jamie","－－－－－确定－－－－－");
+                    pause();
+
+                    break;
+                case KeyEvent.KEYCODE_BACK://返回
+//                    Log.e("jamie","－－－－－返回－－－－－");
+                    break;
+                case KeyEvent.KEYCODE_HOME://房子
+//                    Log.e("jamie","－－－－－房子－－－－－");
+                    break;
+                case KeyEvent.KEYCODE_MENU://菜单
+//                    Log.e("jamie","－－－－－菜单－－－－－");
+                    break;
+
+                    default:{
+                        overrided=super.shouldOverrideKeyEvent(view, event);;
+                    }
+
+
+            }
+
+            return overrided;
         }
     }
 
@@ -80,7 +160,12 @@ public class XwalkWebViewActivity extends AppCompatActivity {
 
 
         Timber.d("URL="+proxy+url);
-        mXwalkView.load(proxy+url, null);
+        mXwalkView.loadUrl(proxy+url, null);
+
+
+        loadingView=findViewById(R.id.loadingPanel);
+        loadingView.setVisibility(View.VISIBLE);
+
     }
 
     @Override
